d898e647588b030cf6773da7efc61a6fdb4bfa1c
velo1203 <hokitoki1203@gmail.com>
2024-02-03 12:21:34 +0900

    auth model created

--- internal/requestmodel/authModel.js
+++ internal/requestmodel/authModel.js
@@ -1,9 +1,23 @@
 class registerRequestModel {
+    constructor({ email, password, username }) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+    }
+    validate() {
+        if (!this.email || !this.password || !this.username === "") {
+            throw Error("email, username and password are required");
+        }
+        return true;
+    }
+}
+
+class loginRequestModel {
     constructor({ email, password }) {
         this.email = email;
         this.password = password;
     }
-    vaildate() {
+    validate() {
         if (!this.email || !this.password) {
             throw Error("email and password are required");
         }
@@ -11,4 +25,4 @@ class registerRequestModel {
     }
 }
 
-module.exports = registerRequestModel;
+module.exports = { loginRequestModel, registerRequestModel }; // 정의된 라우터 모듈을 내보냅니다.
